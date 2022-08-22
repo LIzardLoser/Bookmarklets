b0f37a85b54a03d70ba1820e4a24a03534740f5e
LIzardLoser <46665722+LIzardLoser@users.noreply.github.com>
2022-08-22 15:17:59 -0400

    Update testdty.js

--- testdty.js
+++ testdty.js
@@ -1,12 +1,11 @@
-//source code
-var page = window.open();
+var page = window.open('');
 
 page.document.body.style.backgroundColor = "black";
 page.document.body.style.color = "white";
 page.document.title = "HEYYO";
 
 var buttoon = page.document.createElement("button");
-buttoon.innerHTML = "Titties";
+buttoon.innerHTML = "Alert";
 buttoon.style.backgroundColor = "black";
 buttoon.style.color = "white";
 buttoon.style.border = "2px solid orange";
@@ -22,12 +21,53 @@ buttoon.onmouseleave = function(){
 };
 
 buttoon.onclick = function(){
-    page.alert("I <3 TITTIES")
+    setTimeout(() => {
+        page.alert(textb.value);
+    }, secondstowait.value);
 }
 
 page.document.body.append(buttoon);
 
+var textb = page.document.createElement("textarea");
+textb.value = "Type Here";
+textb.style.border = "2px solid orange";
+textb.style.backgroundColor = "black";
+textb.style.color = "white";
+
+textb.onclick = function(){
+    if (textb.value == "Type Here"){
+        textb.value = "";
+    }
+    if (textb.value == ""){
+        textb.value = "Type Here"
+    }
+};
+page.document.body.append(textb);
+
+var secondstowait = page.document.createElement('input');
+secondstowait.value = "Please choose how many MS to wait";
+secondstowait.style.border = "2px solid orange";
+secondstowait.style.backgroundColor = "black";
+secondstowait.style.color = "white";
+
+secondstowait.onclick = function(){
+    secondstowait.value = ""
+    if (secondstowait.value == ""){
+        secondstowait.value = "Please choose how many MS to wait"
+    }
+};
+
+page.document.body.appendChild(secondstowait);
+
 var h = page.document.createElement("h1");
 h.innerHTML = "also Titties";
 page.document.body.append(h);
-//source code
+
+/*
+javascript:(function(){
+    bookmarklet = document.createElement("script");
+    bookmarklet.setAttribute("type", "text/javascript");
+    bookmarklet.setAttribute("src", "(input raw code here)");
+    document.getElementsByTagName('body')[0].appendChild(bookmarklet);
+  })
+*/
